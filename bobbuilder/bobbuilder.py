149ec92d656720275a4d2efbb1be98f8e63156a2
Attilio Chiavegatti <attiliochiavegatti@gmail.com>
2024-02-16 16:05:31 -0300

    refactor of the ligand classifier

--- bobbuilder/bobbuilder.py
+++ bobbuilder/bobbuilder.py
@@ -138,7 +138,7 @@ for decoration_i, decoration in enumerate(input_data['decorations'], 1):
         adjacency_matrix = fragment_adj_matrix,
         rotatable_bonds = rotatable_bonds,
         numconfs=(len(fragment_coordinates)*len(rotatable_bonds)),
-        threshold=0.960
+        threshold=.960
     )
 
     if args.verbose:
@@ -159,137 +159,148 @@ for decoration_i, decoration in enumerate(input_data['decorations'], 1):
                 xyz_file = build_xyz_file(fragment_elements, coordinates)
                 f.write(xyz_file)
     
+    # replacement level
     for replacement_i, core_replace_atom in enumerate(decoration['replace at']):
         if args.verbose:
             print(f"Running replacement {decoration_i}.{replacement_i}")
           
-    # map added coordinates to actual atom numbers
-    if replacement_i > 0:
-        for coordinate in added_fragments_coordinates:
-            atom_number = np.where(np.all(core_coordinates_.round(4) == coordinate, axis=1))[0][0]
-            added_fragments_atom_numbers = np.append(added_fragments_atom_numbers, atom_number)
-        ignore_atoms_ = np.append(core_atoms_, added_fragments_atom_numbers)
+        # map added coordinates to actual atom numbers
+        if replacement_i > 0:
+            for coordinate in added_fragments_coordinates:
+                atom_number = np.where(np.all(core_coordinates_.round(4) == coordinate, axis=1))[0][0]
+                added_fragments_atom_numbers = np.append(added_fragments_atom_numbers, atom_number)
+            ignore_atoms_ = np.append(core_atoms_, added_fragments_atom_numbers)
+
+            if args.verbose:
+                print(f"New atoms added at positions: {added_fragments_atom_numbers+1}\n")
+            
+        else:
+            ignore_atoms_ = core_atoms_.copy()
+
+        # map current core_replace_atom to the actual core atom number
+        core_replace_atom_coords = core_coordinates[core_replace_atom].round(4)
+        core_replace_atom_ = np.where(np.all(core_coordinates_.round(4) == core_replace_atom_coords, axis=1))[0][0]
 
         if args.verbose:
-            print(f"New atoms added at positions: {added_fragments_atom_numbers+1}\n")
-        
-    else:
-        ignore_atoms_ = core_atoms_.copy()
+            print(f"Original core atom numbers: {core_replace_atom+1}\nUpdated core atom number: {core_replace_atom_+1}")
+            xyz_file = build_xyz_file(core_elements_, core_coordinates_)
+            with open(f'tmp.decor{decoration_i}-{replacement_i}_core0.xyz', mode='w') as f:
+                f.write(xyz_file)
 
-    # map current core_replace_atom to the actual core atom number
-    core_replace_atom_coords = core_coordinates[core_replace_atom].round(4)
-    core_replace_atom_ = np.where(np.all(core_coordinates_.round(4) == core_replace_atom_coords, axis=1))[0][0]
+        # check if core is a terminal-type replacement
+        # if not, remove all neighbours of the replacement point (except for core and core-neighbours)
+        core_neighbours = np.array(find_neighbors(core_adj_matrix_, core_replace_atom_, excluded_atoms=ignore_atoms_))
+        core_neighbours = np.delete(core_neighbours, np.where(core_neighbours == core_replace_atom_))
 
-    if args.verbose:
-        print(f"Original core atom numbers: {core_replace_atom+1}\nUpdated core atom number: {core_replace_atom_+1}")
-        xyz_file = build_xyz_file(core_elements_, core_coordinates_)
-        with open(f'tmp.decor{decoration_i}-{replacement_i}_core0.xyz', mode='w') as f:
-            f.write(xyz_file)
+        if len(core_neighbours) > 0:
+            if args.verbose:
+                print('Neighbours to the connection point will be removed')
+                print(f"Removing atoms {core_neighbours+1}")
 
-    # check if core is a terminal-type replacement
-    # if not, remove all neighbours of the replacement point (except for core and core-neighbours)
-    core_neighbours = np.array(find_neighbors(core_adj_matrix_, core_replace_atom_, excluded_atoms=ignore_atoms_))
-    core_neighbours = np.delete(core_neighbours, np.where(core_neighbours == core_replace_atom_))
+            core_replace_atom_ -= len(np.where(core_neighbours < core_replace_atom_)[0])
 
-    if len(core_neighbours) > 0:
+            for i,atom in enumerate(core_atoms_):
+                core_atoms_[i] -= len(np.where(core_neighbours < atom)[0])
+
+            core_coordinates_ = np.delete(core_coordinates_, core_neighbours, axis=0)
+            core_elements_ = np.delete(core_elements_, core_neighbours, axis=0)
+
         if args.verbose:
-            print('Neighbours to the connection point will be removed')
-            print(f"Removing atoms {core_neighbours+1}")
+            xyz_file = build_xyz_file(core_elements_, core_coordinates_)
+            with open(f'tmp.decor{decoration_i}-{replacement_i}_core1.xyz', mode='w') as f:
+                f.write(xyz_file)
+            print(f"Core atoms: {core_atoms_+1}")
+            print(f"Replace at: {core_replace_atom_+1}")
 
-        core_replace_atom_ -= len(np.where(core_neighbours < core_replace_atom_)[0])
-
-        for i,atom in enumerate(core_atoms_):
-            core_atoms_[i] -= len(np.where(core_neighbours < atom)[0])
-
-        core_coordinates_ = np.delete(core_coordinates_, core_neighbours, axis=0)
-        core_elements_ = np.delete(core_elements_, core_neighbours, axis=0)
-
-    if args.verbose:
-        xyz_file = build_xyz_file(core_elements_, core_coordinates_)
-        with open(f'tmp.decor{decoration_i}-{replacement_i}_core1.xyz', mode='w') as f:
-            f.write(xyz_file)
-        print(f"Core atoms: {core_atoms_+1}")
-        print(f"Replace at: {core_replace_atom_+1}")
-
-    coordinates_all = []
-    fragments_all = []
-    intersection_volume_all = []
-    elements_join = np.concatenate([core_elements_, fragment_elements])
-
-    for frag_conf_coordinates in fragment_conformers:
-        fragment_coordinates_ = frag_conf_coordinates.copy()
-        fragment_elements_ = fragment_elements.copy()
-
-        # translate core to origin
-        core_translation = core_coordinates_[core_replace_atom_].copy()
-        _core_coordinates = core_coordinates_.copy()
-        _core_coordinates -= core_translation
-
-        # translate fragment to origin
-        fragment_coordinates_ -= fragment_coordinates_[fragment_connecting_atoms]
-
-        #identify the core-axis
-        axis_point2_atom = np.where(core_adj_matrix_[core_replace_atom_] == 1)[0][0]
-        core_axis = _core_coordinates[core_replace_atom_] - _core_coordinates[axis_point2_atom]
-
-        # set fragment axis coordinates
-        # len(axis) = 1, take the coordinates of this atom
-        # len(axis) > 1, then take the average of the atoms coordinates
-
-        if len(fragment_bond_axis_atoms) == 1:
-            fragment_axis = fragment_coordinates_[fragment_bond_axis_atoms[0]]
-        elif len(fragment_bond_axis_atoms) > 1:
-            _ = np.array([fragment_coordinates_[i] for i in fragment_bond_axis_atoms])
-            fragment_axis = np.mean(_, axis=0)
-
-        # rotate the fragment such the fragment axis matches the core axis
-        R, t = kabsch_algorithm(
-            fragment_axis.reshape(3,-1),
-            core_axis.reshape(3,-1),
-            center=False
-        )
-        fragment_coordinates_ = (R @ fragment_coordinates_.T).T
-        
-        # remove the core atom
-        _core_coordinates = np.delete(_core_coordinates, core_replace_atom_, axis=0)
-        _core_elements = np.delete(core_elements_, core_replace_atom_)
-        
-        # join core elements with fragment elements
-        elements_join = np.concatenate([_core_elements, fragment_elements_])
-
-        # rotate the fragment 360 degrees about the fragment axis
-        # find the optimal positioning for the rigid fragment
-        # by reducing the vdW spheres superposition
-        rotation_steps = 50
-        rotation_stepsize_rad = np.radians(360) / rotation_steps
-
+        coordinates_all = []
+        fragments_all = []
+        intersection_volume_all = []
         all_distances = np.array([])
-        for rotation in range(rotation_steps-1):
-            quat = axisangle_to_q(core_axis, rotation_stepsize_rad)
-            for i, atom_coordinates in enumerate(fragment_coordinates_):
-                new_coordinates = qv_mult(quat, tuple(atom_coordinates))
-                fragment_coordinates_[i] = new_coordinates
+        elements_join = np.concatenate([core_elements_, fragment_elements])
 
-            coordinates_join = np.vstack([_core_coordinates, fragment_coordinates_])
-            distances = pdist(coordinates_join, 'euclidean')
+        # test each conformer for the best fit with core
+        for frag_conf_coordinates in fragment_conformers:
+            fragment_coordinates_ = frag_conf_coordinates.copy()
+            fragment_elements_ = fragment_elements.copy()
 
-            # if args.verbose:
-            #     xyz_file = build_xyz_file(elements_join, coordinates_join)
-            #     with open(f'd{decoration_i}r{replacement_i}_rotations.xyz', mode='a') as f:
-            #         f.write(xyz_file)
+            # translate core to origin
+            core_translation = core_coordinates_[core_replace_atom_].copy()
+            _core_coordinates = core_coordinates_.copy()
+            _core_coordinates -= core_translation
 
-            if (distances > .900).all():
+            # translate fragment to origin
+            fragment_coordinates_ -= fragment_coordinates_[fragment_connecting_atoms]
+
+            #identify the core-axis
+            axis_point2_atom = np.where(core_adj_matrix_[core_replace_atom_] == 1)[0][0]
+            core_axis = _core_coordinates[core_replace_atom_] - _core_coordinates[axis_point2_atom]
+
+            # set fragment axis coordinates
+            # len(axis) = 1, take the coordinates of this atom
+            # len(axis) > 1, then take the average of the atoms coordinates
+            if len(fragment_bond_axis_atoms) == 1:
+                fragment_axis = fragment_coordinates_[fragment_bond_axis_atoms[0]]
+            elif len(fragment_bond_axis_atoms) > 1:
+                _ = np.array([fragment_coordinates_[i] for i in fragment_bond_axis_atoms])
+                fragment_axis = np.mean(_, axis=0)
+
+            # rotate the fragment such the fragment axis matches the core axis
+            R, t = kabsch_algorithm(
+                fragment_axis.reshape(3,-1),
+                core_axis.reshape(3,-1),
+                center=False
+            )
+            fragment_coordinates_ = (R @ fragment_coordinates_.T).T
+            
+            # remove the core atom
+            _core_coordinates = np.delete(_core_coordinates, core_replace_atom_, axis=0)
+            _core_elements = np.delete(core_elements_, core_replace_atom_)
+            
+            # join core elements with fragment elements
+            elements_join = np.concatenate([_core_elements, fragment_elements_])
+
+            # rotate the fragment 360 degrees about the fragment axis
+            # find the optimal positioning for the rigid fragment
+            # by reducing the vdW spheres superposition
+            rotation_steps = 100
+            rotation_stepsize_rad = np.radians(360) / rotation_steps
+
+            for rotation in range(rotation_steps-1):
+                quat = axisangle_to_q(core_axis, rotation_stepsize_rad)
+                for i, atom_coordinates in enumerate(fragment_coordinates_):
+                    new_coordinates = qv_mult(quat, tuple(atom_coordinates))
+                    fragment_coordinates_[i] = new_coordinates
+
+                coordinates_join = np.vstack([_core_coordinates, fragment_coordinates_])
+                distances = pdist(coordinates_join, 'euclidean')
+
                 all_distances = np.append(all_distances, distances.min())
                 fragments_all.append(fragment_coordinates_.copy())
                 coordinates_all.append(coordinates_join.copy())
-            # elif (distances > .950).all():
-            #     all_distances = np.append(all_distances, distances.min())
-            #     fragments_all.append(fragment_coordinates_.copy())
-            #     coordinates_all.append(coordinates_join.copy())
-            else:
-                all_distances = np.append(all_distances, distances.min())
-                continue
 
+        threshold_955 = (all_distances >= .955)
+        threshold_950 = (all_distances >= .950)
+        threshold_890 = (all_distances >= .890)
+        if args.verbose:
+            print(f"Number of valid geometries (.955 threshold): {threshold_955.sum()}")
+            print(f"Number of valid geometries (.950 threshold): {threshold_950.sum()}")
+            print(f"Number of valid geometries (.890 threshold): {threshold_890.sum()}")
+        if threshold_955.sum() > 0:
+            coordinates_all = [coordinates_all[i] for i in np.where(threshold_955)[0]]
+            fragments_all = [fragments_all[i] for i in np.where(threshold_955)[0]]
+        elif threshold_950.sum() > 0:
+            coordinates_all = [coordinates_all[i] for i in np.where(threshold_950)[0]]
+            fragments_all = [fragments_all[i] for i in np.where(threshold_950)[0]]
+        elif threshold_890.sum() > 0:
+            coordinates_all = [coordinates_all[i] for i in np.where(threshold_890)[0]]
+            fragments_all = [fragments_all[i] for i in np.where(threshold_890)[0]]
+        else:
+            if args.verbose:
+                for threshold in np.arange(0.740,0.955,0.005):
+                    print(f"Threshold: {threshold.round(3):.3f} => {(all_distances >= threshold).sum()} valid geometries")
+            raise ValueError("Impossible to fit this ligand")
+        
+        for fragment_coordinates_ in fragments_all:
             intersection_volumes = sphere_intersection_volumes(
                 _core_coordinates,
                 fragment_coordinates_,
@@ -298,50 +309,47 @@ for decoration_i, decoration in enumerate(input_data['decorations'], 1):
             )
             intersection_volume = np.array(intersection_volumes).sum()
             intersection_volume_all.append(intersection_volume)
+            
+        intersection_volume_all = np.array(intersection_volume_all)
+        optimal_rotation = intersection_volume_all.argmin()
 
-    if args.verbose:
-        print(f"Minimal interdistance found by rotations: {all_distances.min().round(4)}")
-        print(f"No of valid geometries: {len(coordinates_all)}")
-    intersection_volume_all = np.array(intersection_volume_all)
-    optimal_rotation = intersection_volume_all.argmin()
+        best_coordinates = coordinates_all[optimal_rotation] + core_translation
+        best_fragment = fragments_all[optimal_rotation] + core_translation
+        
+        # update core coordinates for the next decoration cycle
+        core_elements_ = elements_join
+        core_coordinates_ = best_coordinates
 
-    best_coordinates = coordinates_all[optimal_rotation] + core_translation
-    best_fragment = fragments_all[optimal_rotation] + core_translation
-    
-    # update core coordinates for the next decoration cycle
-    core_elements_ = elements_join
-    core_coordinates_ = best_coordinates
+        # identify current core atom numbers
+        # then remap core to 1,...,n atom numbers again
+        current_core_atoms = []
+        for atom_idx in core_atoms:
+            atom_coords = core_coordinates[atom_idx].round(4)
+            atom_idx_updated = np.where(np.all(core_coordinates_.round(4) == atom_coords, axis=1))[0][0]
+            current_core_atoms.append(atom_idx_updated)
+        current_core_atoms = np.array(current_core_atoms)
+        
+        if args.verbose:
+            print(f"Current core atom numbers: {current_core_atoms+1}")
 
-    # identify current core atom numbers
-    # then remap core to 1,...,n atom numbers again
-    current_core_atoms = []
-    for atom_idx in core_atoms:
-        atom_coords = core_coordinates[atom_idx].round(4)
-        atom_idx_updated = np.where(np.all(core_coordinates_.round(4) == atom_coords, axis=1))[0][0]
-        current_core_atoms.append(atom_idx_updated)
-    current_core_atoms = np.array(current_core_atoms)
-    
-    if args.verbose:
-        print(f"Current core atom numbers: {current_core_atoms+1}")
+        _ = reorder_xyz(
+            np.concatenate([core_elements_.reshape(-1,1), core_coordinates_.reshape(-1,3)], axis=1),
+            current_core_atoms,
+            fixed_core,
+        )
+        core_atoms_ = fixed_core
+        core_elements_ = _[:,0].reshape(-1).astype(np.str_)
+        core_coordinates_ = _[:,1:].astype(float)
+        core_adj_matrix_ = morfeus.utils.get_connectivity_matrix(core_coordinates_,core_elements_)
 
-    _ = reorder_xyz(
-        np.concatenate([core_elements_.reshape(-1,1), core_coordinates_.reshape(-1,3)], axis=1),
-        current_core_atoms,
-        fixed_core,
-    )
-    core_atoms_ = fixed_core
-    core_elements_ = _[:,0].reshape(-1).astype(np.str_)
-    core_coordinates_ = _[:,1:].astype(float)
-    core_adj_matrix_ = morfeus.utils.get_connectivity_matrix(core_coordinates_,core_elements_)
+        added_fragments_coordinates = np.append(added_fragments_coordinates, best_fragment, axis=0)
+        added_fragments_coordinates = added_fragments_coordinates.round(4)
 
-    added_fragments_coordinates = np.append(added_fragments_coordinates, best_fragment, axis=0)
-    added_fragments_coordinates = added_fragments_coordinates.round(4)
+        if args.verbose:
+            print(f"The core has been renumbered.\nCurrent core atom numbers: {core_atoms_+1}")
 
-    if args.verbose:
-        print(f"The core has been renumbered.\nCurrent core atom numbers:{core_atoms_+1}")
-
-    if args.verbose:
-        xyz_file = build_xyz_file(core_elements_, core_coordinates_)
-        with open(f'tmp.decor{decoration_i}-{replacement_i}_core2.xyz', mode='w') as f:
-            f.write(xyz_file)
+        if args.verbose:
+            xyz_file = build_xyz_file(core_elements_, core_coordinates_)
+            with open(f'tmp.decor{decoration_i}-{replacement_i}_core2.xyz', mode='w') as f:
+                f.write(xyz_file)
         print("Decoration done\n\n")
